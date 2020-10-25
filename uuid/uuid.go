f5faf71b65262ffa2128cb264c39385f65650427
Tushar Shah <twoshark@gmail.com>
2020-10-25 11:11:38 -0400

    no need to loop

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -32,7 +32,7 @@ func Generate(idType IDType) string {
 	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
 		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 
-	for Exists(uuid, idType) {
+	if Exists(uuid, idType) {
 		uuid = Generate(idType)
 	}
 
